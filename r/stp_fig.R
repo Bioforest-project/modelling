2faa67b56a48859cf9d54eb881fc1fc61b5ba6ee
Sylvain Schmitt <sylvain.m.schmitt@gmail.com>
2025-01-24 17:34:31 +0100

    modelling 0.1.0.9023: stp.R option for max lp_

--- r/stp_fig.R
+++ r/stp_fig.R
@@ -3,15 +3,45 @@ stp_fig <- function(
     data_pre,
     data_old,
     fit_stp,
-    lab) {
-  pars_s <- fit_stp$summary(c("thetaInf_s", "tau_s"), "median") %>%
-    separate(variable, c("par", "s", "sitenum"), convert = TRUE) %>%
-    select(-s) %>%
-    pivot_wider(names_from = par, values_from = median)
-  pars_p <- fit_stp$summary(c("dist_p", "lambda_p", "delta_p"), "median") %>%
-    separate(variable, c("par", "p", "plotnum"), convert = TRUE) %>%
-    select(-p) %>%
-    pivot_wider(names_from = par, values_from = median)
+    lab,
+    max_ll = FALSE) {
+  if (max_ll) {
+    max_ll <- which(fit_stp$draws("lp__") == max(fit_stp$draws("lp__")),
+                    arr.ind = TRUE)
+    pars_s <- lapply(
+      c("thetaInf_s", "tau_s"),
+      function(par) {
+        fit_stp$draws(par)[max_ll[1], max_ll[2], ] %>%
+          as_tibble() %>%
+          gather()
+      }
+    ) %>%
+      bind_rows() %>%
+      separate(key, c("chain", "par", "s", "sitenum"), convert = TRUE) %>%
+      select(-s, -chain) %>%
+      pivot_wider(names_from = par, values_from = value)
+    pars_p <- lapply(
+      c("dist_p", "lambda_p", "delta_p"),
+      function(par) {
+        fit_stp$draws(par)[max_ll[1], max_ll[2], ] %>%
+          as_tibble() %>%
+          gather()
+      }
+    ) %>%
+      bind_rows() %>%
+      separate(key, c("chain", "par", "p", "plotnum"), convert = TRUE) %>%
+      select(-p, -chain) %>%
+      pivot_wider(names_from = par, values_from = value)
+  } else {
+    pars_s <- fit_stp$summary(c("thetaInf_s", "tau_s"), "median") %>%
+      separate(variable, c("par", "s", "sitenum"), convert = TRUE) %>%
+      select(-s) %>%
+      pivot_wider(names_from = par, values_from = median)
+    pars_p <- fit_stp$summary(c("dist_p", "lambda_p", "delta_p"), "median") %>%
+      separate(variable, c("par", "p", "plotnum"), convert = TRUE) %>%
+      select(-p) %>%
+      pivot_wider(names_from = par, values_from = median)
+  }
   projs <- data_rec %>%
     select(site, plot, sitenum, plotnum) %>%
     left_join(pars_s, by = join_by(sitenum)) %>%
@@ -21,12 +51,17 @@ stp_fig <- function(
     mutate(stem = stp_pred(rel_year, dist, thetaInf, lambda, tau, delta))
   g <- projs %>%
     ggplot(aes(rel_year, stem)) +
-    geom_boxplot(data = data_old %>% mutate(rel_year = 61),
-                 width = 4, fill = NA) +
-    geom_boxplot(data = data_pre %>% mutate(rel_year = -1),
-                 width = 4, fill = NA) +
+    geom_boxplot(
+      data = data_old %>% mutate(rel_year = 61),
+      width = 4, fill = NA
+    ) +
+    geom_boxplot(
+      data = data_pre %>% mutate(rel_year = -1),
+      width = 4, fill = NA
+    ) +
     geom_point(aes(group = paste(site, plot), col = plot),
-               data = data_rec, alpha = .5) +
+      data = data_rec, alpha = .5
+    ) +
     geom_line(aes(group = paste(site, plot), col = plot)) +
     facet_wrap(~site, scales = "free") +
     theme_bw() +
